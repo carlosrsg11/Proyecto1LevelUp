ddfd128e8a4de7e5c141e6afa0150fd69d3a84c4
Carlos Rodolfo Santisteban Gramajo <49936473+carlosrsg11@users.noreply.github.com>
2022-10-12 21:21:17 -0600

    Delete Proyecto1.js

--- Proyecto1.js
+++
@@ -1,9 +0,0 @@
-const keysAndValues = {3: "carlos", 1: "joel", 2: "adrian", 4: "fernanda", 5: "cristian"};
-
-const nueva = Object.keys(keysAndValues).sort().reduce((contador, key) => {
-    contador[key] = keysAndValues[key];
-    return contador;
-},{});
-
-console.log(Object.keys(nueva))
-console.log(Object.values(nueva))
